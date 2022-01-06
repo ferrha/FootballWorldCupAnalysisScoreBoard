0b202a77f9b9ce6344c5b4197b9d2b291c4dfcc9
ferrha <felix.reverte@hotmail.com>
2022-01-06 18:59:58 +0100

    Refactor GetSummaryByTotalScore. Added comments. Removed prefixes from parameters' name.

--- FootballWorldCupAnalysisScoreBoard/AnalysisScoreBoard.cs
+++ FootballWorldCupAnalysisScoreBoard/AnalysisScoreBoard.cs
@@ -9,21 +9,24 @@ namespace FootballWorldCupAnalysisScoreBoard
 {
     public class AnalysisScoreBoard
     {
-        public OrderedDictionary Games = new OrderedDictionary();
+        public Dictionary<string, int> Games = new Dictionary<string, int>();
 
         ///<summary>
-        ///Method to add a game
+        ///Method to add a game. Creates a FootballTeam for every home and away team.
         ///</summary>
         ///<returns>
         ///</returns>
-        public void AddGame(string pHomeTeamName, int pHomeTeamScore, string pAwayTeamName, int pAwayTeamScore)
+        public void AddGame(string homeTeamName, int homeTeamScore, string awayTeamName, int awayTeamScore)
         {
             try
             {
-                FootballTeam homeTeam = new FootballTeam(pHomeTeamName, pHomeTeamScore);
-                FootballTeam awayTeam = new FootballTeam(pAwayTeamName, pAwayTeamScore);
+                // Creation of the home team object with the team name and the score
+                FootballTeam homeTeamInfo = new FootballTeam(homeTeamName, homeTeamScore);
+                // Creation of the away team object with the team name and the score
+                FootballTeam awayTeamInfo = new FootballTeam(awayTeamName, awayTeamScore);
 
-                Games.Add(homeTeam.TeamName + " " + homeTeam.TeamScore + " - " + awayTeam.TeamName + " " + awayTeam.TeamScore, homeTeam.TeamScore + awayTeam.TeamScore);
+                // Addition of the new game to the dictionary
+                Games.Add(homeTeamInfo.TeamName + " " + homeTeamInfo.TeamScore + " - " + awayTeamInfo.TeamName + " " + awayTeamInfo.TeamScore, homeTeamInfo.TeamScore + awayTeamInfo.TeamScore);
             }
             catch (Exception e)
             {
@@ -31,29 +34,23 @@ namespace FootballWorldCupAnalysisScoreBoard
             }
         }
 
-        ///<summary>
-        ///Method to get a summary of games by total score. Those games with the same total score will be returned ordered by the most recently added to our system.  
+        ///<summary>Method to get a summary of games by total score. Those games with the same total score will be returned ordered by the most recently added to our system.  
         ///</summary>
-        ///<param name="pGames">
-        ///OrderedDictionary with entry data
-        ///</param>
-        ///<returns>
-        ///List of strings with the games added ordered by total score.
-        ///</returns>
-        public List<string> GetSummaryByTotalScore(OrderedDictionary pGames)
+        ///<param name="gamesAdded">Dictionary with entry data. Key in string format and Value in int format.</param>
+        ///<returns>List of strings with the games added ordered by total score.</returns>
+        ///<exception cref="Exception">General exception in the method</exception>
+        public List<string> GetSummaryByTotalScore(Dictionary<string,int> gamesAdded)
         {
             try
             {
-
+                // Initialization of the sorted list with the results to return
                 List<string> sortedList = new List<string>();
 
-                var orderedDictionary = pGames.Cast<System.Collections.DictionaryEntry>().ToDictionary(c => c.Key, d => d.Value);
-
-                var newOrderedDictionary = orderedDictionary.Reverse();
-
-                var finalOrderedDictionary = newOrderedDictionary.OrderByDescending(r => r.Value);
-
-                foreach (var item in finalOrderedDictionary)
+                /*
+                Loop over the games added ordering by total score and those games with the same total score will be ordered by the most recently added
+                and store the element in the sortedList
+                 */
+                foreach (KeyValuePair<string,int> item in gamesAdded.Reverse().OrderByDescending(r => r.Value))
                 {
                     sortedList.Add((string)item.Key);
                 }
@@ -69,7 +66,7 @@ namespace FootballWorldCupAnalysisScoreBoard
     }
 
     ///<summary>
-    ///Football team definition
+    ///Football team definition. Creates an object FootballTeam with TeamName and TeamScore properties.
     ///</summary>
     ///<returns>
     ///</returns>
